7fafe0ba43d6f1b4f63b281e502df1c8a557845e
LukasBauer <ge97joj@mytum.de>
2025-06-06 10:08:18 +0200

    adding values to constants

--- src/constants.jl
+++ src/constants.jl
@@ -12,7 +12,7 @@ This value is taken from the PDG 2020 recommended values of the fundamental phys
 - The Review of Particle Physics: P.A. Zyla et al. (Particle Data Group), Prog. Theor. Exp. Phys. 2020, 083C01 (2020)
 
 """
-ELECTRON_MASS
+const ELECTRON_MASS::Float64 = 0.51099895000
 
 """
    MUON_MASS 
@@ -25,7 +25,7 @@ This value is taken from the PDG 2020 recommended values of the fundamental phys
 - The Review of Particle Physics: P.A. Zyla et al. (Particle Data Group), Prog. Theor. Exp. Phys. 2020, 083C01 (2020)
 
 """
-MUON_MASS
+const MUON_MASS::Float64 = 105.6583755
 
 """
   ALPHA 
@@ -38,4 +38,4 @@ This value is taken from the PDG 2020 recommended values of the fundamental phys
 - The Review of Particle Physics: P.A. Zyla et al. (Particle Data Group), Prog. Theor. Exp. Phys. 2020, 083C01 (2020)
 
 """
-ALPHA
+const ALPHA::Float64 = 0.0072973525
